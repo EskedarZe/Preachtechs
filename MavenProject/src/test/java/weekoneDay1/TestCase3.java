9e9a7a8e7ffbd407745273ad9f585e0425b9e88d
Eskedar <e.zewdu@yahoo.com>
2021-05-31 16:09:29 -0700

    Test case 3: Deleting a Lead

--- MavenProject/src/test/java/weekoneDay1/TestCase3.java
+++ MavenProject/src/test/java/weekoneDay1/TestCase3.java
@@ -10,16 +10,30 @@ public class TestCase3 {
 	public static void main(String[] args) {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver=new ChromeDriver();
+		
+		// Launch browser
 		driver.get("http://iarchtaps.com:8080/opentaps");
+		// enter username
 		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		// enter passowrd
 		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		// click on Log in
 		driver.findElement(By.className("decorativeSubmit")).click();
+		// Click CRM/CFA link
 		driver.findElement(By.linkText("CRM/SFA")).click();
+		//  Click Leads link
 		driver.findElement(By.linkText("Leads")).click();
+		//  Click find leads
 		driver.findElement(By.linkText("Find Leads")).click();
-		driver.findElement(By.xpath("(//span[@class='x-tab-strip-text '])[1]")).click();
-		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys("4087526598");
-		driver.findElement(By.linkText("Find Leads")).click();
+		//  Click on Phone
+		driver.findElement(By.xpath("((//ul[@class='x-tab-strip x-tab-strip-top'])/li)[2]")).click();
+		// Enter Phone number
+		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys("4087526596");
+		//  Click the find leads button
+		driver.findElement(By.xpath("(//button[@class='x-btn-text'])[7]")).click();
+		//need to capture lead ID
+		//  Click on first resulting lead
+		driver.findElement(By.xpath("((//div[@class='x-grid3-cell-inner x-grid3-col-firstName'])[2])/a")).click();
 		
 		
 	}
